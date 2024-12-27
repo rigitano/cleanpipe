6b70c6d385e9cf11a78c9a3e35ca64487db5a8c5
henrique rigitano <henrique.de-carvalho-rigitano@etu.univ-lyon1.fr>
2024-12-27 18:59:44 +0100

    box full of that debug 12

--- cleanpipe/systemCreation.py
+++ cleanpipe/systemCreation.py
@@ -44,7 +44,7 @@ def pdb2box_full_of_that(s_pdbfile, s_forceField):
     #manipulate the GRO file to create a 5x5x5 box and fill it with copyes of the molecule
     command = f"gmx insert-molecules -ci {s_outPathAndName}_just1mol.gro -nmol 1000 -rot xyz -box 5 5 5 -o {s_outPathAndName}.gro"
     captured_output = bricksFileSystem.run_and_capture(command)
-    bricksFileSystem.delete("{s_outPathAndName}_just1mol.gro")#now we have the final gro with a lot of molecules. its time to delete the initial one
+    bricksFileSystem.delete(f"{s_outPathAndName}_just1mol.gro")#now we have the final gro with a lot of molecules. its time to delete the initial one
 
 
     bricksFileSystem.delete(f"{s_outPathAndName}.gro")# now that we have the filled box gro, the 1 molecule gro can be deleted
