111217039f028c3bf6c3a0e07ae15806a0c998f1
henrique rigitano <henrique.de-carvalho-rigitano@etu.univ-lyon1.fr>
2024-12-31 18:22:48 +0100

    visualization 2

--- cleanpipe/__init__.py
+++ cleanpipe/__init__.py
@@ -1,19 +1,9 @@
-from .algelin import find_new_atom_coord
-from .bricksAtoms import add_acetyl_to_Nterminus, add_amide_to_Cterminus
-from .bricksFileSystem import check_extention, get_filename_without_extension, get_file_location, check_folder, get_all_files_with_certain_extention, get_single_gro, get_single_top, get_all_itps, delete, run_and_capture, create_folder, diagnostics
-from .bricksMD import (
-	ensure_original_directory,
-	pdb2system,
-	solvate_and_neutralize,
-	make_realistic,
-    production,
-	hbonds,
-	sasa,
-	rama,
-	dssp 
-)
-from .bricksChem import download_and_clean_pdb, create_peptide
-from .bricksTopEdit import getMoleculeName, getSystemName, replaceWordInsideDirective, replaceMoleculeName, setSystemName, update_molecule_quantity, decompose_TOP_file_into_TOP_and_ITPs, remove_posres_inclusion, insert_text_before_directive
-from .systemCreation import pdb2box_full_of_that, pdb2molecule_in_solvent, void2peptide_in_solvent
-from .visualisations import plot_hbonds, plot_sasa, plot_ramachandran, plot_dssp
+from .algelin import *
+from .bricksAtoms import *
+from .bricksFileSystem import *
+from .bricksMD import *
+from .bricksChem import *
+from .bricksTopEdit import *
+from .systemCreation import *
+from .visualisations import *
 
