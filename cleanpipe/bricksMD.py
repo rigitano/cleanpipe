0135771064470cbb1c2404072ec38aca74ec8d64
henrique rigitano <henrique.de-carvalho-rigitano@etu.univ-lyon1.fr>
2024-12-31 13:09:30 +0100

    run 13

--- cleanpipe/bricksMD.py
+++ cleanpipe/bricksMD.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 import functools
 import multiprocessing
+import platform
 
 def ensure_original_directory(func):
     """
@@ -251,6 +252,76 @@ def make_realistic(s_systemFolder,s_groups_to_monitor_separately, s_temperature)
     bricksFileSystem.run_and_capture(f"gmx mdrun -deffnm npt")
     os.chdir(f"..")
 
+def processor_diagnistics():
+    """
+    cores
+    threads
+    cluster
+    """
+    print(f"Number of processors: {multiprocessing.cpu_count()}")
+    print(f"Number of threads: {os.cpu_count()}")
+    print(f"Cluster: {platform.node()}")
+
+#def benchmark():
+
+#def benchmark_rome():
+
+
+
+
+@ensure_original_directory
+def production(s_systemFolder,n_nanoseconds):
+    """
+    Runs molecular dynamics simulation for the given system folder.
+
+    Parameters:
+    s_systemFolder (str): The folder containing the system files for the simulation.
+    """
+
+    print(f"\nCLEANPIPE MESSAGE called production({s_systemFolder},{n_nanoseconds})\n")
+
+    n_nanoseconds = int(n_nanoseconds)
+
+    # setup mdp
+    s_module_folder = os.path.dirname(__file__)
+    s_mdp_folder = os.path.join(s_module_folder,"mdp")
+
+    # go inside the system folder and create 4_PROD
+    os.chdir(f"{s_systemFolder}")#change current folder to the system folder
+    s_topName = bricksFileSystem.get_single_top(".") #get top name on the system folder
+    bricksFileSystem.create_folder("4_PROD")#create folder
+    os.chdir(f"4_PROD")#go to created folder
+
+    #initial simulation
+    bricksFileSystem.run_and_capture(f"gmx grompp -f {s_mdp_folder}/prod_continuation_Vr_Cr.mdp -c ../3_NPT/npt.gro -p ../{s_topName} -o prod_0000to0001.tpr -maxwarn 3")
+    bricksFileSystem.run_and_capture(f"gmx mdrun -v -deffnm prod_0000to0001")
+
+    #extend the simulation, use the tpbconv tool to extend the .tpr file
+    bricksFileSystem.run_and_capture(f"gmx convert-tpr -s prod_0000to0001.tpr -extend {str(n_nanoseconds*1000-1)} -o prod_0001to{n_nanoseconds:04d}.tpr")
+    bricksFileSystem.run_and_capture(f"gmx mdrun -v -deffnm prod_0001to{n_nanoseconds:04d} -cpi prod_0000to0001.cpt")  # Continue the simulation from the checkpoint file
+
+    #xxx room for improvement in the processing configurations, for example -nt 8
+
+    #recenter (dont go to edges) and fit (appear to just jitter standing still)
+    bricksFileSystem.run_and_capture(f"printf '1\n0' | gmx trjconv -s prod.tpr -f prod.xtc -o prod_centered.xtc -center -pbc mol")
+    bricksFileSystem.run_and_capture(f"printf '1\n0' | gmx trjconv -s prod.tpr -f prod_centered.xtc -o prod_fitted.xtc -fit progressive")
+
+    
+    #  fool proff alternative
+    #subprocess.run(["gmx", "trjconv", "-s", "prod.tpr", "-f", "prod.xtc", "-o", "prod_centered.xtc", "-center", "-pbc", "mol"], input="1\n0\n", text=True)
+    #subprocess.run(["gmx", "trjconv", "-s", "prod.tpr", "-f", "prod_centered.xtc", "-o", "prod_fitted.xtc", "-fit", "progressive"], input="1\n0\n", text=True)
+
+    os.chdir(f"..")
+
+
+#def production_rome():
+
+
+#def production_fep():
+
+#def production_fep_rome():
+
+
 
 def hbonds(s_xtc,s_tpr):
     """
@@ -321,50 +392,7 @@ def dssp(s_xtc,s_gro):
     bricksFileSystem.run_and_capture(f"gmx dssp -f {s_xtc} -s {s_gro} -o {s_out_folder}/dssp.dat -hmode dssp")
 
     
-@ensure_original_directory
-def run_md_simulation(s_systemFolder):
-    """
-    Runs molecular dynamics simulation for the given system folder.
-
-    Parameters:
-    s_systemFolder (str): The folder containing the system files for the simulation.
-    """
-
-
-    # setup mdp
-    s_module_folder = os.path.dirname(__file__)
-    s_mdp_folder = os.path.join(s_module_folder,"mdp")
-
-    # go inside the system folder and create 4_PROD
-    os.chdir(f"{s_systemFolder}")#change current folder to the system folder
-    s_topName = bricksFileSystem.get_single_top(".") #get top name on the system folder
-    bricksFileSystem.create_folder("4_PROD")#create folder
-    os.chdir(f"4_PROD")#go to created folder
-
-    #simulation
-    bricksFileSystem.run_and_capture(f"gmx grompp -f {s_mdp_folder}/prod_continuation_Vr_Cr.mdp -c ../3_NPT/npt.gro -p ../{s_topName} -o prod.tpr -maxwarn 3")
-    bricksFileSystem.run_and_capture(f"gmx mdrun -v -deffnm prod -nt {multiprocessing.cpu_count()}")
-
-    #recenter (dont go to edges) and fit (appear to just jitter standing still)
-    bricksFileSystem.run_and_capture(f"printf '1\n0' | gmx trjconv -s prod.tpr -f prod.xtc -o prod_centered.xtc -center -pbc mol")
-    bricksFileSystem.run_and_capture(f"printf '1\n0' | gmx trjconv -s prod.tpr -f prod_centered.xtc -o prod_fitted.xtc -fit progressive")
-
-    
-    #  fool proff alternative
-    #subprocess.run(["gmx", "trjconv", "-s", "prod.tpr", "-f", "prod.xtc", "-o", "prod_centered.xtc", "-center", "-pbc", "mol"], input="1\n0\n", text=True)
-    #subprocess.run(["gmx", "trjconv", "-s", "prod.tpr", "-f", "prod_centered.xtc", "-o", "prod_fitted.xtc", "-fit", "progressive"], input="1\n0\n", text=True)
-
-    os.chdir(f"..")
 
 
 
-#def run_rome():
 
-
-#def benchmark():
-
-#def benchmark_rome():
-
-#def run_fep():
-
-#def run_fep_rome():
