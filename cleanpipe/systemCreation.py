3b46e770924deec12c0afd3f6d08db123f964367
henrique rigitano <henrique.de-carvalho-rigitano@etu.univ-lyon1.fr>
2025-01-07 14:15:10 +0100

    lipid shape 3

--- cleanpipe/systemCreation.py
+++ cleanpipe/systemCreation.py
@@ -71,16 +71,15 @@ def pdb2box_full_of_that(s_pdbfile, s_forceField):
 
 def pdb2molecule_in_solvent(s_pdbfile, s_outSytemName, s_solvent, s_forceField, s_boxSize):
     """
-    usage example:
-    cl.pdb2molecule_in_solvent("1LZ1.pdb", "1LZ1_in_water", "tip3p", "charmm36-jul2022", "3 3 3")
-    cl.pdb2molecule_in_solvent("1LZ1.pdb", "1LZ1_in_octane", "box_full_of_octn", "charmm36-jul2022", "3 3 3")
-    
-
     s_pdbfile       : string with the pdb name. for example "insulin.pdb", this will be the main molecule in the system.
     s_outSytemName  : string with the name of the system, for example "alaHW". a folder with that name will be created, and inside it, all the files, for example: alaHW.gro and alaHW.top
     s_solvent       : choose a water model, for example as "tip3p", or a folder, for example "octn_filledbox". The folder have to contain a system with a solvent box, in other words, it has to contain a octn_filledbox.gro and a octn.itp
     s_forceField    : one of the gromacs recognized force fields, for example "charmm36-jul2022"
     s_boxSize       : string with x y z sizes, for example "3 3 3"
+
+    example:
+    cl.pdb2molecule_in_solvent("1LZ1.pdb", "1LZ1_in_water", "tip3p", "charmm36-jul2022", "3 3 3")
+    cl.pdb2molecule_in_solvent("1LZ1.pdb", "1LZ1_in_octane", "box_full_of_octn", "charmm36-jul2022", "3 3 3")
     """
 
     # check if the filename inside s_pdbfile is valid
@@ -97,51 +96,11 @@ def pdb2molecule_in_solvent(s_pdbfile, s_outSytemName, s_solvent, s_forceField, 
 
 
 
-
-def void2peptide_in_solvent(s_peptideName, s_systemName, s_nTerminusCAP, s_aminoacids, s_cTerminusCAP, l_phi, l_psi_im1, s_solvent, s_forceField, s_boxSize):
-    """
-    usage example:
-    cl.void2peptide_in_solvent("poliA","poliA_in_water","acyl","AAAAAA","amide",[-57.8,-57.8,-57.8,-57.8,-57.8,-57.8],[-47.0,-47.0,-47.0,-47.0,-47.0,-47.0],"tip3p","charmm36-jul2022", "5.1 5.1 5.1")
-    cl.void2peptide_in_solvent("poliA","poliA_in_octane","acyl","AAAAAA","amide",[-57.8,-57.8,-57.8,-57.8,-57.8,-57.8],[-47.0,-47.0,-47.0,-47.0,-47.0,-47.0],"box_full_of_octn","charmm36-jul2022", "5.1 5.1 5.1")
-
-
-    will create the peptide and the entire system out of nowere (no input files required)
-    a folder containing the system will be created. this will be done using just the function arguments 
-
-    s_peptideName   : name of the peptide that will be created, for example alaH
-    s_systemName    : string with the name of the system, for example "alaHW". a folder with that name will be created, and inside it, all the files, for example: alaHW.gro and alaHW.top
-    s_nTerminusCAP  : N terminus CAP. there are only the folowing options: "acyl" , none
-    s_aminoacids    : string of aminoacid one letter code, for example "AAAAGGAALL"
-    s_cTerminusCAP  : this are the C terminus CAP. there are only the folowing options:: "amide", none
-    l_phi           : vector with angles
-    l_psi_im1       : vector with angles
-    s_solvent       : choose a water model, for example as "tip3p", or a folder, for example "octn_filledbox". The folder have to contain a system with a solvent box, in other words, it has to contain a octn_filledbox.gro and a octn.itp
-    s_forceField    : one of the gromacs recognized force fields, for example "charmm36-jul2022"
-    s_boxSize       : string with x y z sizes, for example "3 3 3"
-     
-    """
-
-    # create  pdb file containing only a peptide. in this case the pdb will be temporary, and deleted after the conversion to top and gro
-    bricksChem.create_peptide(f"{s_peptideName}.pdb", s_nTerminusCAP, s_aminoacids, s_cTerminusCAP, l_phi, l_psi_im1)
-   
-    # create gro and top from pdb. then add the box size to the gro. this is the my improved gromacs version that create the system in a new folder
-    bricksMD.pdb2system(f"{s_peptideName}.pdb",s_systemName,s_forceField,s_boxSize, b_addterminal = False)# this last parameter was set to False so not to add termini, as they are already present in the peptide in this case)
-
-    # temporary pdb of the peptide is not necessary anymore
-    bricksFileSystem.delete(f"{s_peptideName}.pdb")
-
-    # add solvent to the system. this is my improved gromacs, the imput is a folderthere are 2 possible options here: tip3p or filled box
-    bricksMD.solvate_and_neutralize(s_systemName,s_solvent,s_forceField)
-
-    # set the the name of the system in the top file 
-    bricksTopEdit.setSystemName(f"{s_systemName}/{s_systemName}.top", f"{s_systemName} (custom peptide, insterted in solution made using {s_solvent})" )
-
-
-def create_lipid_cylinder(lipidsList, s_radius, s_thickness, s_box, s_outSysName, s_ff_location):
+def create_tube_in_vacum(lipidsList, s_radius, s_thickness, s_box, s_outSysName, s_ff_location):
     """
 
     example:
-    cl.create_lipid_cylinder([["DOPC","0.98","0.98","0.68"],["TO","0.02","0.02","0.68"]], s_radius= "18", s_thickness="2", s_box="50 50 50", s_outSysName="hiout", s_ff_location="/home/bioinformatician/repositories/lipid_sorting/TS2CG-Setup-Pipeline/top/Martini3+NLs.LIB")    
+    cl.create_tube_in_vacum([["DOPC","0.98","0.98","0.68"],["TO","0.02","0.02","0.68"]], s_radius= "18", s_thickness="2", s_box="50 50 50", s_outSysName="tube_in_vacum", s_ff_location="/home/bioinformatician/repositories/lipid_sorting/TS2CG-Setup-Pipeline/top/Martini3+NLs.LIB")    
     """
 
     # Create a folder to store the output system
@@ -171,11 +130,55 @@ End
     """
 
     # Save the description content to a temporary file
-    with open("description.str", "w") as temp_file:
+    with open("temporary_description.str", "w") as temp_file:
         temp_file.write(description_content)
 
     s_tool = "/home/bioinformatician/repositories/lipid_sorting/TS2CG-Setup-Pipeline/PCG"
-    bricksFileSystem.run_and_capture(f"{s_tool} -str description.str -Bondlength 0.2 -LLIB {s_ff_location} -function analytical_shape -defout {s_outPathAndName}")
+    bricksFileSystem.run_and_capture(f"{s_tool} -str temporary_description.str -Bondlength 0.2 -LLIB {s_ff_location} -function analytical_shape -defout {s_outPathAndName}")
 
     # Delete the temporary file
-    #bricksFileSystem.delete("description.str")
+    bricksFileSystem.delete("temporary_description.str")
+
+
+def create_vesicle_in_vacum(lipidsList, s_radius, s_thickness, s_box, s_outSysName, s_ff_location):
+    """
+
+    example:
+    cl.create_vesicle_in_vacum([["DOPC","0.98","0.98","0.68"],["TO","0.02","0.02","0.68"]], s_radius= "18", s_thickness="2", s_box="50 50 50", s_outSysName="vesicle_in_vacum", s_ff_location="/home/bioinformatician/repositories/lipid_sorting/TS2CG-Setup-Pipeline/top/Martini3+NLs.LIB")    
+    """
+
+    # Create a folder to store the output system
+    bricksFileSystem.run_and_capture(f"mkdir {s_outSysName}")
+    s_outPathAndName = f"{s_outSysName}/{s_outSysName}"
+
+    #copy the forcefiled to the system folder, and uptate the forcefield location
+    bricksFileSystem.run_and_capture(f"cp -r \"{s_ff_location}\" \"{s_outSysName}\"")
+    s_ff_location = os.path.basename(s_ff_location)
+    s_ff_location = f"{s_outSysName}/{s_ff_location}"
+
+    # Read the lipids list and format it so it can be pasted in the description file
+    lipids_list_ready_to_paste = "\n".join(["     ".join(map(str, lipid)) for lipid in lipidsList])
+
+    description_content = f"""
+[Lipids List]
+Domain 0
+{lipids_list_ready_to_paste}
+End
+
+[Shape Data]
+ShapeType Sphere
+Radius {s_radius}
+Thickness {s_thickness}
+Box {s_box}
+End
+    """
+
+    # Save the description content to a temporary file
+    with open("temporary_description.str", "w") as temp_file:
+        temp_file.write(description_content)
+
+    s_tool = "/home/bioinformatician/repositories/lipid_sorting/TS2CG-Setup-Pipeline/PCG"
+    bricksFileSystem.run_and_capture(f"{s_tool} -str temporary_description.str -Bondlength 0.2 -LLIB {s_ff_location} -function analytical_shape -defout {s_outPathAndName}")
+
+    # Delete the temporary file
+    bricksFileSystem.delete("temporary_description.str")
