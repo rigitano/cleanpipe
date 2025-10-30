ac0fc03cb37d9df3a49ba7f15a01c6af246be708
hrigitano <henrique.rigitano@ibcp.com>
2025-10-30 15:57:17 +0100

    highlight what I want

--- cleanpipe/tcl/selection_highlight.tcl
+++ cleanpipe/tcl/selection_highlight.tcl
@@ -30,12 +30,33 @@ mol addrep top
 
 #SELECTED IS RED
 
-# The selection
-set text "(resname PRO CYS) or (name CA) or (element S) or (index 1 2 3)"
+# here is an example of a complex selection, just for reference
+# mol selection {(resname PRO CYS) or (name CA) or (element S) or (index 1 2 3)}
 
 
 # represent selection as red Licorice
-mol selection $text
+mol selection {resname SED_WILL_REPLACE_THIS_1}
+mol representation Licorice 0.3 12 12
+mol color ColorID 1 ;# red
+mol material Opaque
+mol addrep top
+
+# represent selection as red Licorice
+mol selection {name SED_WILL_REPLACE_THIS_2}
+mol representation Licorice 0.3 12 12
+mol color ColorID 1 ;# red
+mol material Opaque
+mol addrep top
+
+# represent selection as red Licorice
+mol selection {element SED_WILL_REPLACE_THIS_3}
+mol representation Licorice 0.3 12 12
+mol color ColorID 1 ;# red
+mol material Opaque
+mol addrep top
+
+# represent selection as red Licorice
+mol selection {index SED_WILL_REPLACE_THIS_4}
 mol representation Licorice 0.3 12 12
 mol color ColorID 1 ;# red
 mol material Opaque
@@ -44,14 +65,12 @@ mol addrep top
 
 
 
-
-
 # Write index file, to use the selection in gromacs. xxx I dont know if index starts at 0, if they are the same, or if formatting is necessary inside the ndx file
 
-set sel [atomselect top $text]
-set ch [open "~/Desktop/custom_selection.ndx" w]
-puts $ch [$sel get index]
-close $ch
+#set sel [atomselect top $text]
+#set ch [open "~/Desktop/custom_selection.ndx" w]
+#puts $ch [$sel get index]
+#close $ch
 
 
 
