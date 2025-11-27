00d646f9a365af3f5bb7fd7976de0646643e31a2
hrigitano <henrique.rigitano@ibcp.com>
2025-11-27 14:51:45 +0100

    hbonds improved

--- cleanpipe/tcl/hbond_between_proteins_and_solvent.tcl
+++ cleanpipe/tcl/hbond_between_proteins_and_solvent.tcl
@@ -9,15 +9,15 @@ display projection orthographic
 # See protein as licorice
 mol selection {not water}
 mol representation Licorice 0.2 12 12
-mol color Type ;# by atom type
+mol color ResID ;# before: Type
 mol material Opaque ;# before, it was Goodsell.
 mol addrep top
 
 # Add a cartoon representation for the backbone
 mol selection {protein}
 mol representation NewCartoon
-mol color ColorID 2 ;# grey
-mol material Opaque ;# before, it was Goodsell
+mol color ResID ;# before: ColorID 2
+mol material Transparent ;# before, it was Goodsell
 mol addrep top
 
 # Visualize molecules within hydrogen bond range of the protein
@@ -30,14 +30,26 @@ mol addrep top
 
 # Visualize hydrogen bonds using Hbonds drawing method (between protein and water)
 mol selection {(not water) or (water and within 3.5 of (not water))}
-mol representation Hbonds
+mol representation Hbonds 3.3 50.0 58
 mol color ColorID 27 ;# magenta
 mol material Opaque
 mol addrep top
 
 # Visualize hydrogen bonds using Hbonds drawing method (between atoms on the protein backbone)
 mol selection {name N H CO O}
-mol representation Hbonds
+mol representation Hbonds 3.3 50.0 58
 mol color ColorID 27 ;# magenta
+mol material Opaque
+mol addrep top
+
+mol selection {name N}
+mol representation VDW 0.2 24
+mol color ColorID 0 ;# blue
+mol material Opaque
+mol addrep top
+
+mol selection {name O}
+mol representation VDW 0.2 24
+mol color ColorID 1 ;# red
 mol material Opaque
 mol addrep top
