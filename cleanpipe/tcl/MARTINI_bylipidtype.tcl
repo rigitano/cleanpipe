249d45222c065abf63bfeb718837dd70a9595a00
hrigitano <henrique.rigitano@ibcp.com>
2026-06-16 17:26:32 +0200

    improved colors of common membrane lipids

--- cleanpipe/tcl/MARTINI_bylipidtype.tcl
+++ cleanpipe/tcl/MARTINI_bylipidtype.tcl
@@ -62,7 +62,7 @@ mol color ColorID 2 ;# grey
 mol addrep top
 
 
-
+# CHOL
 mol representation Licorice 2.6 50 50
 set text "resname CHOL"
 mol selection $text
@@ -70,29 +70,54 @@ mol material AOChalky
 mol color ColorID 11 ;# purple
 mol addrep top
 
+# PC
+
 mol representation Licorice 2.4 50 50
 mol selection {resname POPC}
 mol material AOChalky
-mol color ColorID 10 ;# cyan
+mol color ColorID 23 ;# blue 2
 mol addrep top
 
 mol representation Licorice 2.4 50 50
 mol selection {resname DPPC}
 mol material AOChalky
-mol color ColorID 3 ;# orange
+mol color ColorID 0 ;# blue
 mol addrep top
 
 mol representation Licorice 2.4 50 50
 mol selection {resname DSPC}
 mol material AOChalky
-mol color ColorID 9 ;# pink
+mol color ColorID 6 ;# silver
 mol addrep top
 
 mol representation Licorice 2.4 50 50
 mol selection {resname DOPC}
 mol material AOChalky
+mol color ColorID 15 ;# iceblue
+mol addrep top
+
+mol representation Licorice 2.4 50 50
+mol selection {resname DBPC}
+mol material AOChalky
+mol color ColorID 21 ;# cyan 2
+mol addrep top
+
+mol representation Licorice 2.4 50 50
+mol selection {resname DLPC}
+mol material AOChalky
+mol color ColorID 10 ;#  cyan
+mol addrep top
+
+# SSM
+
+mol representation Licorice 2.4 50 50
+mol selection {resname SSM}
+mol material AOChalky
 mol color ColorID 7 ;# green
 mol addrep top
+
+
+# PE
 
 mol representation Licorice 2.4 50 50
 mol selection {resname DOPE}
@@ -101,46 +126,48 @@ mol color ColorID 4 ;# yellow
 mol addrep top
 
 mol representation Licorice 2.4 50 50
-mol selection {resname DOPS}
-mol material AOChalky
-mol color ColorID 11 ;# purple
-mol addrep top
-
-mol representation Licorice 2.4 50 50
 mol selection {resname POPE}
 mol material AOChalky
-mol color ColorID 15 ;# lightblue
+mol color ColorID 14 ;# ochre
+mol addrep top
+
+
+# PS
+
+mol representation Licorice 2.4 50 50
+mol selection {resname DOPS}
+mol material AOChalky
+mol color ColorID 1 ;# red
 mol addrep top
 
 mol representation Licorice 2.4 50 50
 mol selection {resname POPS}
 mol material AOChalky
-mol color ColorID 13 ;# lightpink
+mol color ColorID 9 ;# pink
+mol addrep top
+
+
+
+
+# PI
+
+mol representation Licorice 2.4 50 50
+mol selection {resname SAPI}
+mol material AOChalky
+mol color ColorID 3 ;# orange
 mol addrep top
 
 mol representation Licorice 2.4 50 50
-mol selection {resname SSM}
+mol selection {resname POPI}
 mol material AOChalky
-mol color ColorID 12 ;# limegreen
-mol addrep top
-
-mol representation Licorice 2.4 50 50
-mol selection {resname DBPC}
-mol material AOChalky
-mol color ColorID 14 ;# 
-mol addrep top
-
-mol representation Licorice 2.4 50 50
-mol selection {resname DLPC}
-mol material AOChalky
-mol color ColorID 21 ;# 
+mol color ColorID 31 ;# orange 2
 mol addrep top
 
 
 
 
 
-
+ 
 
 
 #SOLUTION
